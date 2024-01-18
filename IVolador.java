3a59a28e4cc25bdd1559a9a295b687600226a018
KaaiicDias <151720923+KaaiicDias@users.noreply.github.com>
2024-01-18 15:26:48 +0100

    Update IVolador.java

--- IVolador.java
+++ IVolador.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public interface IVolador {
     void despegar() throws AlturaInsuficienteException;
 
-    double calcularAlturaMaxima(int velocidad, int tiempo) throws AlturaInsuficienteException;
+    void calcularAlturaMaxima(int velocidad, int tiempo) throws AlturaInsuficienteException;
 
     double calcularDistanciaRecorrida(int velocidad, int tiempo);
 
