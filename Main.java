455c5dabfaa2c8f81d45447b4c5b99100e445842
KaaiicDias <151720923+KaaiicDias@users.noreply.github.com>
2024-01-18 15:27:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-public class Main {
+public class MainDinosaurios {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dino d = new Dino("Cachalote",60);
+        CaracteristicaDino C1 = new CaracteristicaDino("aa", 2,"c1",10);
+        d.agregarCaracteristicas(C1);
+        d.mostrarCaracteristicas();
+        d.eliminarCaracteristicas("desc");
+        d.eliminarCaracteristicas("desc1");
+
+        d.despegar();
+        d.calcularAlturaMaxima(10,6);
+        d.despegar();
+        System.out.println(d.calcularDistanciaRecorrida(5,10));
     }
 }
